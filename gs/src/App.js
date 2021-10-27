b24fc6b4d56b4a3c8d652f3338b239ad91a030ba
JoseRenan29 <gasidan07@gmail.com>
2021-10-27 23:47:33 +0800

    Not Updated

--- gs/src/App.js
+++ gs/src/App.js
@@ -1,28 +1,5 @@
 import './App.css';
 import RegController from './pages/registrar/registrarController';
-<<<<<<< HEAD
-import AdminController from './pages/admin/AdminController';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom' 
-
-function App() {
-  return (
-    
-   <Router> 
-    <div>
-      <Switch>
-
-        <Route exact path ="/">
-        <RegController/>
-        </Route> 
-      
-        <Route> 
-          <AdminController/>    
-        </Route>
-
-      </Switch>
-    </div>
-    </Router> 
-=======
 import AdminController from './pages/admin/adminController';
 import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
 
@@ -48,7 +25,6 @@ function App() {
     </div>
     </Router>
 
->>>>>>> f67fdeb218822e03bb1530a5985694d685e89770
   );
 }
 
