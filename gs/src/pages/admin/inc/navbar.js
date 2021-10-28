1a91c88097b0d451a307eae14f0125e3cf87ae1a
thirdy454 <gayaresthird@gmail.com>
2021-10-28 08:42:00 +0800

    para kay renan

--- gs/src/pages/admin/inc/navbar.js
+++ gs/src/pages/admin/inc/navbar.js
@@ -9,6 +9,7 @@ class Navbar extends Component {
         return (
 
             <div>
+
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container">
                         <Link className="navbar-brand" to="/About_us">GGS</Link>
@@ -18,29 +19,29 @@ class Navbar extends Component {
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 
-                            <li class="nav-item dropdown">
-          <Link class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            username
-          </Link>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><Link class="dropdown-item" href="#">Profile</Link></li>
-            <li><Link class="dropdown-item" href="#">Help</Link></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><Link class="dropdown-item" href="#">Logout</Link></li>
-          </ul>
-        </li>
-       
+                                <li class="nav-item dropdown">
+                                    <Link class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        username
+                                    </Link>
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><Link class="dropdown-item" href="#">Profile</Link></li>
+                                        <li><Link class="dropdown-item" href="#">Help</Link></li>
+                                        <li><hr class="dropdown-divider" /></li>
+                                        <li><Link class="dropdown-item" href="#">Logout</Link></li>
+                                    </ul>
+                                </li>
+
                             </ul>
 
                         </div>
                     </div>
                 </nav>
-            
-             
+
+
 
             </div>
 
-            
+
 
         );
 
