31a91e2576ed9ccb5a228e702f861b03ac5fda16
thirdy454 <gayaresthird@gmail.com>
2021-10-28 19:44:43 +0800

    goods dito ang navbar

--- gs/src/pages/admin/adminController.js
+++ gs/src/pages/admin/adminController.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Component } from 'react';
-import Navbar_Homepage from "./inc/navbar";
+import Navbar from "./inc/navbar";
 import About_us from "./Content/About_us";
-import Sidebar from './inc/sidebar';
+
 import Homepage_Controller from "./Content/Homepage";
 import Students from "./students/students";
 import AddTeacher from "./teacher/addTeacher";
-
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import DeanConfig from "./dean/dean_config";
 import RegistrarConfig from "./registrar/registrar_config";
 import GuardinConfig from "./guardian/guardian_config";
+import AdminAccount from "./admin_account/admin_account";
 
 class AdminController extends Component{
 
@@ -20,16 +20,18 @@ class AdminController extends Component{
               
                 <div>
                   
-                    <Navbar_Homepage/>
+                    <Navbar/>
                   
                         <Switch>
-                            <Route path ="/About_us" component ={About_us}/>
+                            <Route path ="/About_usCofig" component ={About_us}/>
                             <Route path ="/homepage_Controller" component ={Homepage_Controller}/>
                             <Route path ="/students_config" component = {Students}/>
                             <Route path ="/teacher_config" component = {AddTeacher}/>
                             <Route path ="/dean_config" component = {DeanConfig}/>
                             <Route path ="/registar_config" component = {RegistrarConfig}/>
-                            <Route path ="/guardian_config" component = {GuardinConfig}/>                           
+                            <Route path ="/guardian_config" component = {GuardinConfig}/> 
+                            <Route path ="/admin_config" component = {AdminAccount}/>                           
+
                         </Switch>
 
                         
